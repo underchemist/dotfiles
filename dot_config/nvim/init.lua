40db084600efdc7d81ec4c4facfae7a0800fb0ff
Sebastien Tremblay-Johnston <stremblay-johnston@sparkgeo.com>
2026-04-08 20:33:50 -0700

    update nvim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,7 +2,9 @@
 vim.pack.add({
 	'https://github.com/neovim/nvim-lspconfig',
 	'https://github.com/mason-org/mason.nvim',
-	'https://github.com/mfussenegger/nvim-lint'
+	'https://github.com/mfussenegger/nvim-lint',
+        'https://github.com/ibhagwan/fzf-lua',
+        'https://github.com/nvim-tree/nvim-web-devicons',
 	})
 
 -- config
@@ -12,6 +14,7 @@ require("mason").setup()
 require("lint").linters_by_ft = {
   python = {"ruff", "mypy"}
 }
+require("fzf-lua").setup()
 
 -- opts
 vim.g.mapleader = " "
@@ -25,3 +28,13 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.hlsearch = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.number = true
+
+-- keymaps
+-- fzf-lua
+vim.api.nvim_set_keymap("n", "<C-\\>", [[<Cmd>lua require"fzf-lua".buffers()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-k>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-p>", [[<Cmd>lua require"fzf-lua".files()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-l>", [[<Cmd>lua require"fzf-lua".live_grep()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-g>", [[<Cmd>lua require"fzf-lua".grep_project()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<F1>", [[<Cmd>lua require"fzf-lua".help_tags()<CR>]], {})
