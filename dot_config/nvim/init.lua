88150dc315ac23db02c7812d6ea62ff0614d1c3f
Sebastien Tremblay-Johnston <stremblay-johnston@sparkgeo.com>
2026-04-15 09:07:30 -0700

    update nvim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,23 +1,43 @@
+-- set mapleader
+vim.g.mapleader = " "
+vim.g.localmapleader = " "
+
 -- plugins
 vim.pack.add({
 	'https://github.com/neovim/nvim-lspconfig',
 	'https://github.com/mason-org/mason.nvim',
-	'https://github.com/mfussenegger/nvim-lint',
         'https://github.com/ibhagwan/fzf-lua',
         'https://github.com/nvim-tree/nvim-web-devicons',
-	})
+        { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+        { src=  "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main", build = ":TSUpdate", name = "nvim-treesitter"},
+    })
 
 -- config
+require("nvim-treesitter").setup({})
+require("nvim-treesitter").install({
+    "rust",
+    "python",
+    "markdown",
+    "lua",
+})
+-- colorscheme
+require("catppuccin").setup({
+    flavour = 'macchiato'
+})
 vim.lsp.enable('pyright')
 require('vim._core.ui2').enable({enable=true})
 require("mason").setup()
-require("lint").linters_by_ft = {
-  python = {"ruff", "mypy"}
-}
 require("fzf-lua").setup()
 
+-- autocmds
+-- tree-sitter
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = {'<filetype>'},
+    callback = function() vim.treesitter() end,
+})
+
 -- opts
-vim.g.mapleader = " "
+vim.cmd.colorscheme  "catppuccin-nvim"
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.encoding = "utf-8"
@@ -29,6 +49,12 @@ vim.opt.hlsearch = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.number = true
+vim.opt.signcolumn = "yes"
+vim.opt.showmode = false
+vim.opt.iskeyword:append("-") -- include '-' in words
+vim.opt.selection = "inclusive"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 -- keymaps
 -- fzf-lua
