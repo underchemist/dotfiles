b6a83a2a5a9902416e76c856f44cedf059bce80f
Sebastien Tremblay-Johnston <stremblay-johnston@sparkgeo.com>
2026-05-11 11:01:02 -0700

    feat(nvim): formatting and adjusting fzf keymaps

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,70 +1,79 @@
-require('vim._core.ui2').enable({enable=true})
+require("vim._core.ui2").enable({ enable = true })
 -- set mapleader
 vim.g.mapleader = " "
 vim.g.localmapleader = " "
 
 -- plugins
 vim.pack.add({
-        { src = "https://github.com/neovim/nvim-lspconfig", branch = "main" },
+	{ src = "https://github.com/neovim/nvim-lspconfig", branch = "main" },
 	{ src = "https://github.com/mason-org/mason.nvim", branch = "main" },
-        { src = "https://github.com/mason-org/mason-lspconfig.nvim", branch = "main" },
-        { src = "https://github.com/ibhagwan/fzf-lua", branch = "main" },
-        { src = "https://github.com/nvim-tree/nvim-web-devicons", branch = "main" },
-        { src = "https://github.com/lewis6991/gitsigns.nvim", branch = "main" },
-        { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
-        { src =  "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main", build = ":TSUpdate", name = "nvim-treesitter"},
-        { src = "https://github.com/alker0/chezmoi.vim", branch = "main", name = "chezmoi.vim" },
-        { src = "https://github.com/kylechui/nvim-surround", version = vim.version.range("4.x"), name = "nvim-surround" },
-    })
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim", branch = "main" },
+	{ src = "https://github.com/ibhagwan/fzf-lua", branch = "main" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons", branch = "main" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim", branch = "main" },
+	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+	{
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+		name = "nvim-treesitter",
+	},
+	{ src = "https://github.com/alker0/chezmoi.vim", branch = "main", name = "chezmoi.vim" },
+	{ src = "https://github.com/kylechui/nvim-surround", version = vim.version.range("4.x"), name = "nvim-surround" },
+	{ src = "https://github.com/mfussenegger/nvim-dap", branch = "main", name = "nvim-dap" },
+	{ src = "https://github.com/mfussenegger/nvim-dap-python", branch = "main", name = "nvim-dap-python" },
+})
 
 -- config
 require("nvim-treesitter").setup({})
 require("nvim-treesitter").install({
-    "rust",
-    "python",
-    "markdown",
-    "lua",
-    "yaml",
+	"rust",
+	"python",
+	"markdown",
+	"lua",
+	"yaml",
 })
 -- colorscheme
 require("catppuccin").setup({
-    flavour = 'macchiato'
+	flavour = "macchiato",
 })
 -- lsp
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "basedpyright", "yamlls", "ruff", "stylua" }
+	ensure_installed = { "basedpyright", "yamlls", "ruff", "stylua" },
 })
 require("fzf-lua").setup()
 
 -- autocmds
 -- tree-sitter
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = {'<filetype>'},
-    callback = function() vim.treesitter() end,
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "<filetype>" },
+	callback = function()
+		vim.treesitter()
+	end,
 })
 
 -- markdown formatting
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "markdown",
-    callback = function()
-        vim.opt_local.wrap = true
-        vim.opt_local.linebreak = true
-        vim.opt_local.spell = true
-        vim.opt_local.spelllang = "en_ca"
-        vim.opt_local.breakindent = true
-    end,
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.linebreak = true
+		vim.opt_local.spell = true
+		vim.opt_local.spelllang = "en_ca"
+		vim.opt_local.breakindent = true
+	end,
 })
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "yaml", "toml" }, 
-    callback = function()
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 2
-        vim.opt_local.softtabstop = 2
-    end,
+	pattern = { "yaml", "toml" },
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+	end,
 })
 -- opts
-vim.cmd.colorscheme  "catppuccin-nvim"
+vim.cmd.colorscheme("catppuccin-nvim")
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.encoding = "utf-8"
@@ -83,13 +92,18 @@ vim.opt.selection = "inclusive"
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.undofile = true
-vim.opt.shortmess:append("I")  -- disable intro 
+vim.opt.shortmess:append("I") -- disable intro
+vim.opt.complete = ".,w,b,o"
+vim.opt.completeopt = "menuone,noselect,fuzzy"
+vim.opt.autocomplete = true
+vim.opt.autocompletedelay = 250 -- ms
+
 -- keymaps
 -- fzf-lua
 vim.api.nvim_set_keymap("n", "<C-\\>", [[<Cmd>lua require"fzf-lua".buffers()<CR>]], {})
-vim.api.nvim_set_keymap("n", "<C-k>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-b>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {})
 vim.api.nvim_set_keymap("n", "<C-p>", [[<Cmd>lua require"fzf-lua".files()<CR>]], {})
 vim.api.nvim_set_keymap("n", "<C-l>", [[<Cmd>lua require"fzf-lua".live_grep()<CR>]], {})
 vim.api.nvim_set_keymap("n", "<C-g>", [[<Cmd>lua require"fzf-lua".grep_project()<CR>]], {})
 vim.api.nvim_set_keymap("n", "<F1>", [[<Cmd>lua require"fzf-lua".help_tags()<CR>]], {})
-
+vim.api.nvim_set_keymap("n", "<C-k>", [[<Cmd>lua require"fzf-lua".keymaps()<CR>]], {})
